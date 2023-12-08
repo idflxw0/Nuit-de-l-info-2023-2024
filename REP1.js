cf2721258ba84027ac4d970ce16b5e5676903d8d
riadfrz <riadfarouzi@gmail.com>
2023-12-08 02:51:41 +0100

    a

--- REP1.js
+++ REP1.js
@@ -1,4 +1,6 @@
 // script.js
+
+
 const questions = [
     {
         question: "Parmi ces 5 propositions, quel est le gaz à effet de serre ?",
@@ -6,7 +8,7 @@ const questions = [
             { text: "CO2, dioxyde de carbone", correct: false },
             { text: "CH4, méthane", correct: false },
             { text: "N2O, protoxyde d’azote", correct: false },
-            { text: "Tous ces gazs", correct: false }
+            { text: "Tous ces gazs", correct:  true },
         ],
         introduction: "Ces 4 gazs sont différents type de gaz à effet de serre à longue durée de vie\n" +
             "Le CO, le plus connu provient de la combustion des énergies fossiles, déforestation, …\n" +
@@ -17,16 +19,18 @@ const questions = [
     // Ajoutez d'autres questions de la même manière
 
     {
-        question: "Quelle est la capitale de la France ?",
+        question: "Parmi ces propositions, laquelle est une solution au changement climatique ?",
         answers: [
-            { text: "Paris", correct: true },
-            { text: "Berlin", correct: false },
-            { text: "Madrid", correct: false },
-            { text: "Londres", correct: false }
+            { text: "Réduire les émissions du CO2", correct: false },
+            { text: "Planter des arbres", correct: false },
+            { text: "Préserver et restaurer les écosystèmes", correct: true },
+            { text: "l’utilisation de la bioénergie", correct: false }
         ],
-        introduction: "Paris est la capitale de la France. C'est une ville connue pour sa riche histoire et sa culture.",
+        introduction: "Planter des arbres et l’utilisation de la bioénergie pourrait être une solution mais en réalité, beaucoup plus de dioxyde de carbone est émis, et réduire les émissions de CO2 n’est pas une bonne idée étant donné que nous serons dépendant des GES dans le futur.",
         nextButton: "Suivant"
     },
+
+
 ];
 
 let currentQuestionIndex = 0;
@@ -42,6 +46,12 @@ function displayQuestion() {
     answerButtons.forEach((button, index) => {
         button.innerHTML = currentQuestion.answers[index].text;
     });
+}
+
+function getcorrectAnswer(position) {
+    const currentQuestion = questions[currentQuestionIndex];
+
+    return currentQuestion.answers[position].correct;
 }
 
 function checkAnswer(isCorrect) {
